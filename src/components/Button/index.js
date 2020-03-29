ad370267157b4a204f89c82bc2df99c333cb666f
Renan Henrique <henriquebasshvf@gmail.com>
2020-03-29 13:33:52 -0300

    add load indicator lib and adjust button to show with a loading progress

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { ActivityIndicator } from 'react-native'
+import { WaveIndicator } from 'react-native-indicators'
 import * as C from './styles'
 import PropTypes from 'prop-types'
 
-const Button = ({ title, press, confirm }) => {
+const Button = ({ title, press, confirm, is_loading }) => {
   return (
     <C.Container confirm={confirm} onPress={() => press()}>
-      <C.Text confirm={confirm}>{title}</C.Text>
+      {!is_loading ? (
+        <C.Text confirm={confirm}>{title}</C.Text>
+      ) : (
+        <WaveIndicator color="white" size={30} />
+      )}
     </C.Container>
   )
 }
@@ -13,13 +19,15 @@ const Button = ({ title, press, confirm }) => {
 Button.defaultProps = {
   title: '',
   confirm: false,
-  press: () => {}
+  is_loading: false,
+  press: () => {},
 }
 
 Button.propTypes = {
   title: PropTypes.string.isRequired,
   confirm: PropTypes.bool.isRequired,
-  press: PropTypes.func.isRequired
+  is_loading: PropTypes.bool.isRequired,
+  press: PropTypes.func.isRequired,
 }
 
 export default Button
