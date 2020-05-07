ab594dfa2bfb13491f139ce48ff2f4517fc9785d
Renan H <henriquebasshvf@gmail.com>
2020-05-06 21:40:50 -0300

    update

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -2,16 +2,16 @@ import React, { useState, useEffect } from 'react'
 import * as C from './styles'
 import PropTypes from 'prop-types'
 
-export default function Avatar({ size, url }) {
+export default function Avatar({ size, url, teste }) {
   return <C.Avatar size={size} url={url} />
 }
 
 Avatar.defaultProps = {
   size: 32,
-  url: 'https://pickaface.net/gallery/avatar/20141025_004121_918_Developer.png'
+  url: 'https://pickaface.net/gallery/avatar/20141025_004121_918_Developer.png',
 }
 
 Avatar.propTypes = {
   size: PropTypes.number.isRequired,
-  url: PropTypes.string.isRequired
+  url: PropTypes.string.isRequired,
 }
